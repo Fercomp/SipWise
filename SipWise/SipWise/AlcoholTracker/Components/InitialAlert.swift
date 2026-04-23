1d6fe613adebaa6db893a4fb4ce696d1956dbfd6
Fercomp <fer20comp@gmail.com>
2026-04-23 08:54:29 -0300

    Changing InitialAlert text

--- SipWise/SipWise/AlcoholTracker/Components/InitialAlert.swift
+++ SipWise/SipWise/AlcoholTracker/Components/InitialAlert.swift
@@ -5,5 +5,5 @@
 //  Created by Fernando Santos de Souza on 10/04/26.
 //
 
-let displayText = 
-"Este aplicativo fornece apenas estimativas matemáticas baseadas em fórmulas padrão. Ele não substitui um bafômetro real ou exame de sangue. Para saber mais sobre como esses cálculos funcionam voce pode acessar o icone de duvida (?) no topo da tela. Nunca utilize este aplicativo para determinar se você está apto a dirigir ou operar máquinas. Se beber, não dirija."
+let displayText =
+"This application provides only mathematical estimates based on standard formulas. It does not replace a real breathalyzer or blood test. To learn more about how these calculations work, you can tap the help icon (?) at the top of the screen. Never use this application to determine whether you are fit to drive or operate machinery. If you drink, do not drive."
